5a0374ae3fe891c09990e63a6d3066b8ff33641b
Eagl3ye <vincejaphethloable@gmail.com>
2018-07-15 16:45:59 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,9 @@ async def on_ready():
     print(rin.user.name)
     print(rin.user.id)
     print('------')
-    rin.change_presence(game=discord.Game(name='test'))
 
 @bot.command()
 async def greet(msg):
     await msg.send(":smiley: :wave: Hello, there!")
-    
+    rin.change_presence(game=discord.Game(name='test'))
 rin.run('NDAxNjE2NjkxNTkyNjkxNzEz.DTt_GQ.COQRZfPaW3wT8771iRP5EnxJmAM')
