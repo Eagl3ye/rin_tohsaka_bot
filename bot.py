c4e49c1a227b1fe58a63a505d374da0793c1be06
Eagl3ye <vincejaphethloable@gmail.com>
2018-07-15 18:14:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 import discord
 from discord.ext import commands
 
@@ -5,12 +12,12 @@ rin = commands.Bot(command_prefix='$')
 
 @rin.event
 async def on_ready():
-    print('Logged in as')
-    print(rin.user.name)
-    print(rin.user.id)
-    print('------')
+    print('Logged in as...')
+    print("Bot:",rin.user.name)
+    print("User_ID:",rin.user.id)
+    print('Changing presence...')
     await rin.change_presence(status=discord.Status.idle, activity=discord.Game(name='with Daddy'))
-    
+    print("conn = ", conn)
 @rin.command()
 async def greet(msg):
     await msg.send(":smiley: :wave: Hello, there!")
