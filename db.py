ca3f0105def46f25b8980dcc99d68688b2e09c7f
Eagl3ye <vincejaphethloable@gmail.com>
2018-07-15 21:38:19 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,12 @@
+import psycopg2
 
+conn = psycopg2.connect("dbname=test user=postgres")
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+
+conn.commit()
+
+cur.close()
+conn.close()
