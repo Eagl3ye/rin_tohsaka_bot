dd02ad791960e7a6d334195920f023838f21a1ed
Eagl3ye <vincejaphethloable@gmail.com>
2018-07-16 12:24:02 +0800

    Update db.py

--- db.py
+++ db.py
@@ -7,9 +7,9 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
-#cur.execute("DROP TABLE test;")
+#cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+#cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+cur.execute("DROP TABLE test;")
 
 conn.commit()
 
