ac97b3e5bfa03e394de850bd36aa2f0bc7932171
Eagl3ye <vincejaphethloable@gmail.com>
2018-07-16 10:16:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-import os
-import psycopg2
-
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-import discord
+#Imports
+import discord                                              #DISCORD
 from discord.ext import commands
-
 rin = commands.Bot(command_prefix='r!')
+
+import os                                                   #DATABASE HANDLING
+import psycopg2
+DATABASE_URL = os.environ['DATABASE_URL']                   
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')    
 
 @rin.event
 async def on_ready():
@@ -18,9 +16,15 @@ async def on_ready():
     print('Changing presence...')
     await rin.change_presence(status=discord.Status.dnd, activity=discord.Game(name='with Daddy'))
     print("conn = ", conn)
+    
 @rin.command()
 async def greet(msg):
     await msg.send(":smiley: :wave: Hello, there!")
+
+@rin.command()
+async def myid(msg):
+    await msg.send(msg.author)
+    
 @rin.command()
 async def access(msg):
     cur = conn.cursor()
