4f3f693c2054fc7d7d85e7eda9091d8cb3e9dfd3
Vince Loable <vincejaphethloable@gmail.com>
2018-07-17 15:17:54 +0800

    create unique usr_id

--- db.py
+++ db.py
@@ -5,13 +5,13 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-#cur.execute("DROP TABLE kidz;")
-
-try:
-	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text, mono integer);")
-	print("\n\nCREATED TABLE NAMED kidz...\n\n")
-except psycopg2.DatabaseError:
-	pass
+cur.execute("ALTER TABLE kidz;")
+cur.execute("ADD UNIQUE (usr_id);")
+#try:
+#	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text, mono integer);")
+#	print("\n\nCREATED TABLE NAMED kidz...\n\n")
+#except psycopg2.DatabaseError:
+#	pass
 
 conn.commit()
 conn.close()
