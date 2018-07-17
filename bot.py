087705f79c72f8fa5b2d2b17d432643b71b2297c
Vince Loable <vincejaphethloable@gmail.com>
2018-07-17 23:28:33 +0800

    debug

--- bot.py
+++ bot.py
@@ -45,7 +45,8 @@ async def wallet(msg):
 		pass
 	else:
 		cur.execute("SELECT * FROM kidz WHERE usr_id = "+auth)
-		money = int((cur.fetchall())[2])
+		print(money)
+		money = int((cur.fetchall())[1])
 		if money == 0:
 			await msg.send(":credit_card: | **You have no money in your wallet**")
 		else:
