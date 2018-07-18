3963901e77bad02a3ed15a3153bb2667f0f4893b
Vince Loable <vincejaphethloable@gmail.com>
2018-07-18 08:58:12 +0800

    fix

--- db.py
+++ db.py
@@ -5,12 +5,12 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-#cur.execute("DELETE FROM kidz WHERE usr_id = '@Konev#6479';")
-try:
-	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text UNIQUE, mono integer);")
-	print("\n\nCREATED TABLE NAMED kidz...\n\n")
-except psycopg2.DatabaseError:
-	pass
+cur.execute("UPDATE kidz SET mono = 1 WHERE id = 1;")
+#try:
+#	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text UNIQUE, mono integer);")
+#	print("\n\nCREATED TABLE NAMED kidz...\n\n")
+#except psycopg2.DatabaseError:
+#	pass
 
 conn.commit()
 conn.close()
