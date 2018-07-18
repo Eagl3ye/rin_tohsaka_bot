ae307ccdc305aecdea91e4ab36a093b80f0dced9
Vince Loable <vincejaphethloable@gmail.com>
2018-07-18 09:22:41 +0800

    fix

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM kidz ORDER BY id ASC;")
+cur.execute("UPDATE * FROM kidz ORDER BY id ASC;")
 #cur.execute("UPDATE kidz SET mono = 1 WHERE id = 1;")
 #try:
 #	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text UNIQUE, mono integer);")
