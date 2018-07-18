f08031a20885f8a1b66ec83205d9b20d54d8aa89
Vince Loable <vincejaphethloable@gmail.com>
2018-07-18 17:35:47 +0800

    fix

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def wallet(msg):
 		
 		auth = "'%"+str(args[0])[3:-2]+"%';"
 		cur.execute("SELECT mono FROM kidz WHERE usr_id LIKE "+(auth))
-		money = int((cur.fetchall())[0][0])
+		money = (cur.fetchall())
 		if money == 0:
 			await msg.send(":credit_card: | **He/She has no credits in his/her wallet**")
 		elif money == 1:
