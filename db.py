71461609fca9c61cea8125f58efa99a02989b7fc
Vince Loable <vincejaphethloable@gmail.com>
2018-07-18 09:20:01 +0800

    fix

--- db.py
+++ db.py
@@ -5,14 +5,14 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-cur.execute("SELECT id FROM kidz ORDER BY id ASC;")
+cur.execute("SELECT * FROM kidz ORDER BY id ASC;")
 #cur.execute("UPDATE kidz SET mono = 1 WHERE id = 1;")
-try:
-	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text UNIQUE, mono integer);")
-	print("\n\nCREATED TABLE NAMED kidz...\n\n")
-except psycopg2.DatabaseError:
-	conn.rollback()
-	pass
+#try:
+#	cur.execute("CREATE TABLE kidz (id serial PRIMARY KEY, usr_id text UNIQUE, mono integer);")
+#	print("\n\nCREATED TABLE NAMED kidz...\n\n")
+#except psycopg2.DatabaseError:
+#	conn.rollback()
+#	pass
 
 conn.commit()
 conn.close()
