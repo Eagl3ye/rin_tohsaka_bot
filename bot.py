8151f8214b76a7c33e1b7ac405820babf76105d7
Vince Loable <vincejaphethloable@gmail.com>
2018-07-20 16:57:11 +0800

    try

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
 @bot.event
+@bot.has_permissions(manage_messages=True)
 async def on_command_error(msg, error):
 	if isinstance(error, commands.CommandOnCooldown):
 		await msg.send(":clock5: | **COOLDOWN: Retry again in {:.2f}s.**".format(error.retry_after))
@@ -29,10 +30,6 @@ async def on_ready():
 async def now(msg):
 	dnt = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
 	await msg.send("```python\n['SERVER TIME']\n\nAtlantic/Azores +00:00\n#>\t{}```".format(dnt))
-
-@bot.command()
-async def auth(msg, user):
-	await msg.send(user.name)
 
 @bot.command()
 async def wallet(msg, user:str=None):
