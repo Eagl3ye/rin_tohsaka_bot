a1456aa32eeb8dc345aeb20b81a37af688926fd3
Vince Loable <vincejaphethloable@gmail.com>
2018-07-28 16:01:25 +0800

    Changed output image

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -41,8 +41,8 @@ class Utility:
 	@commands.cooldown(1, 5, commands.BucketType.user)
 	async def google(self, msg):
 		async with channel.typing():
-			image = imgkit.from_url('http://google.com', 'out.jpg')
-			await msg.send('', file=discord.File(image))
+			imgkit.from_url('http://google.com', 'out.jpg')
+			await msg.send('', file=discord.File('out.jpg'))
 
 def setup(bot):
     bot.add_cog(Utility(bot))
