4113f807e470150ba19a3a171ab195241e699ed9
Vince Loable <vincejaphethloable@gmail.com>
2018-07-28 13:33:36 +0800

    Changed presence

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import sys														#SYSTEM
 import traceback												#TRACEBACK
 import time														#TIME
 import asyncio													#ASYNCIO
+from itertools import cycle										#ITERTOOLS
 import discord													#DISCORD API
 from discord.ext import commands
 bot = commands.Bot(command_prefix='r!')
@@ -19,6 +20,16 @@ async def on_command_error(msg, error):
 		await msg.send(":clock5: | **COOLDOWN: Retry again in {:.2f}s.**".format(error.retry_after))
 		return
 
+stats_list = ['.', '. .', '. . .']
+async def status_task():
+	await bot.wait_until_ready()
+	stats = cycle(stats_list)
+
+	while not bot.is_closed:
+		stat = next(stats)
+		await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name=stat))
+		await asyncio.sleep(1)
+
 @bot.event
 async def on_ready():
 	print('Logged in as...')
@@ -26,7 +37,6 @@ async def on_ready():
 	print("User_ID:",bot.user.id)
 	print("Connection >> ", conn)
 	print('Changing presence...')
-	await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name='. . .'))
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=[ COGS ]=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 extensions = ['Cogs.economy', 'Cogs.utility', 'Cogs.dev', 'Cogs.games']
@@ -40,4 +50,5 @@ if __name__ == '__main__':
             traceback.print_exc()
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
+bot.loop.create_task(status_task())
 bot.run(BOT_TOKEN)
