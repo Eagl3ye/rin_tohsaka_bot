920e153920179ebe026d0e5fc895181bd628e9e6
Vince Loable <vincejaphethloable@gmail.com>
2018-07-28 14:31:32 +0800

    Fixed loop

--- bot.py
+++ bot.py
@@ -20,36 +20,30 @@ async def on_command_error(msg, error):
 		await msg.send(":clock5: | **COOLDOWN: Retry again in {:.2f}s.**".format(error.retry_after))
 		return
 
-stats_list = ['.', '. .', '. . .']
-async def status_task():
-	await bot.wait_until_ready()
-	stats = cycle(stats_list)
-	while not bot.is_closed:
-		stat = next(stats)
-		await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name=stat))
-		await asyncio.sleep(1)
-
 @bot.event
 async def on_ready():
-	asyncio.get_event_loop().close()
 	print('Logged in as...')
 	print("Bot:",bot.user.name)
 	print("User_ID:",bot.user.id)
 	print("Connection >> ", conn)
 	print('Changing presence...')
-	asyncio.ensure_future(status_task())
-	asyncio.get_event_loop().run_forever()
+	stats_list = ['.', '. .', '. . .']
+	stats = cycle(stats_list)
+	while True:
+		stat = next(stats)
+		await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name=stat))
+		await asyncio.sleep(1)
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=[ COGS ]=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 extensions = ['Cogs.economy', 'Cogs.utility', 'Cogs.dev', 'Cogs.games']
 
 if __name__ == '__main__':
-	for extension in extensions:
-		try:
-			bot.load_extension(extension)
-		except Exception as e:
-			print(f'Failed to load extension {extension}.', file=sys.stderr)
-			traceback.print_exc()
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
 bot.run(BOT_TOKEN)
