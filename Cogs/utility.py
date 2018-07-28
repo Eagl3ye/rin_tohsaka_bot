83910bced79894136044a40d51c03fc695ca2663
Vince Loable <vincejaphethloable@gmail.com>
2018-07-28 11:07:46 +0800

    Remove short_docs, Added brief

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -11,14 +11,14 @@ class Utility:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(name='greet', aliases=['Hello'], description="Greets the User", hidden=False, short_doc="Greets the User")
+	@commands.command(name='greet', aliases=['Hello'], description="Greets the User", hidden=False, brief="Greets the User")
 	# [+] GREET
 	# [|] Greets the User
 	@commands.cooldown(1, 5, commands.BucketType.user)
 	async def greet(self, msg):
 		await msg.send(":smiley: :wave: Hello, there!")
 
-	@commands.command(name='now', aliases=['time','timenow'], description="Displays the current server time", hidden=False, short_doc="Displays the current server time")	
+	@commands.command(name='now', aliases=['time','timenow'], description="Displays the current server time", hidden=False, brief="Displays the current server time")	
 	# [+] NOW
 	# [|] Displays the current server time
 	@commands.cooldown(1, 2, commands.BucketType.user)
@@ -26,7 +26,7 @@ class Utility:
 		clt = time.strftime("%a, %d %b %Y %I:%M:%S %p", time.localtime())
 		await msg.send("```python\n['SERVER TIME']\n\nAsia/Manila UTC +08:00\n#>\t{}```".format(clt))
 
-	@commands.command(name='myid', aliases=['id'], description="Shows the User's unique Discord ID", hidden=False, short_doc="Shows the User's unique Discord ID")
+	@commands.command(name='myid', aliases=['id'], description="Shows the User's unique Discord ID", hidden=False, brief="Shows the User's unique Discord ID")
 	# [+] MYID
 	# [|] Shows the User's unique Discord ID
 	@commands.cooldown(1, 2, commands.BucketType.user)
